f256efcb1856886ce34583828d034cdf925e11b0
marethyu <codingexpert123@gmail.com>
2022-08-26 16:23:32 -0700

    No need for bitmasking and use &=

--- interrupts.go
+++ interrupts.go
@@ -65,7 +65,7 @@ func (ic *Interrupts) Read32(address uint32) uint32 {
 func (ic *Interrupts) Write16(address uint32, data uint16) {
 	switch address {
 	case 0x1f801070:
-		ic.Status = ic.Status & (uint32(data) | 0xffff0000) // when writing to status, 0=Clear Bit, 1=No change
+		ic.Status &= uint32(data) // when writing to status, 0=Clear Bit, 1=No change
 	case 0x1f801074:
 		ic.Mask = (ic.Mask & 0xffff0000) | uint32(data)
 	default:
@@ -76,7 +76,7 @@ func (ic *Interrupts) Write16(address uint32, data uint16) {
 func (ic *Interrupts) Write32(address uint32, data uint32) {
 	switch address {
 	case 0x1f801070:
-		ic.Status = ic.Status & data
+		ic.Status &= data
 	case 0x1f801074:
 		ic.Mask = data
 	}
